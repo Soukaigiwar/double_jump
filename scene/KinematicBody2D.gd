f691bb38eebcc5276289b529750e11b61701ba49
soukaigiwar <sergiomello@gmail.com>
2021-08-16 00:10:47 -0300

    doublejump

--- scene/KinematicBody2D.gd
+++ scene/KinematicBody2D.gd
@@ -1,16 +1,37 @@
 extends KinematicBody2D
 
-var motion = Vector2()
-var vel_x = 1.5
-var gravity = 5
+const SPEED = 200
+const GRAVITY = 30
+const JUMP_POWER = -700
+const FLOOR = Vector2(0, -1)
+
+var on_ground = false
+var double_jump = 0
+var velocity = Vector2()
 
 func _physics_process(delta):
-	motion.y += gravity
-	if Input.is_action_pressed("ui_right"):
-		motion.x = 100 * vel_x
-	elif Input.is_action_pressed("ui_left"):
-		motion.x = -100 * vel_x
+	if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
+		velocity.x = SPEED
+		
+	elif Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
+		velocity.x = -SPEED
 	else:
-		motion.x = 0
-	move_and_slide(motion)
-	pass
+		velocity.x = 0
+
+	if Input.is_action_pressed("ui_jump") and double_jump < 5:
+		velocity.y = JUMP_POWER
+		double_jump += 1
+		print("jump")
+		
+	apply_gravity()
+	velocity = move_and_slide(velocity, FLOOR)
+	velocity.y += GRAVITY
+	
+func apply_gravity():
+	if is_on_floor():
+		on_ground = true
+		double_jump = 0
+	else:
+		on_ground = false
+		
+	
